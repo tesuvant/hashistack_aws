b6a385d0be5cc2f99b4f33476f81a0609586f9d2
Teemu Suvanto <teemu.suvanto_ext@bbraun.com>
2023-07-31 14:13:28 +0200

    edit

--- outputs.tf
+++ outputs.tf
@@ -9,18 +9,35 @@ output "private_key" {
   sensitive = true
 }
 
-output "How_to_SSH" {
-  value = <<TXT
+output "CONVERT_LINE_BREAKS" {
+  value = <<TXT1
   ### CONVERT LINE BREAKS
   echo "...-----END RSA PRIVATE KEY-----\n" | gsed 's/\\n/\
   /g'
+    TXT1
+}
 
+output "ACCESS_BASTION" {
+  value = <<TXT2
   ### BASTION
   ssh -i ${local_file.hashi_cluster.filename} ${var.ssh_user}@${aws_instance.bastion.public_ip}
+    TXT2
+}
 
+output "PORT_FORWARDING_CONSUL" {
+  value = <<TXT3
   ### PORT FORWARDING TO ACCESS CONSUL
   ssh -i ${local_file.hashi_cluster.filename} -J ${var.ssh_user}@${aws_instance.bastion.public_ip} ${var.ssh_user}@${aws_instance.servers.0.private_ip} -L 8500:127.0.0.1:8500
   point browser to...
   127.0.0.1:8500
-    TXT
+    TXT3
+}
+
+output "PORT_FORWARDING_TRAEFIK" {
+  value = <<TXT3
+  ### PORT FORWARDING TO ACCESS TRAEFIK
+  ssh -i ${local_file.hashi_cluster.filename} -J ${var.ssh_user}@${aws_instance.bastion.public_ip} ${var.ssh_user}@${aws_instance.traefik.0.private_ip} -L 8080:127.0.0.1:8080
+  point browser to...
+  127.0.0.1:8080
+    TXT3
 }
